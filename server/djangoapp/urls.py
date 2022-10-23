45edd724e75bc65a85bcddb39b597ad6f698926d
Fadi <eng_fadi77@hotmail.com>
2022-10-23 20:50:18 +0200

    1st part unfinished

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
+    path(route='about/', view=views.about, name='about'),
 
     # path for about view
 
